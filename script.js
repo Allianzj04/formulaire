939d0028f00aebf61155dc1eda7cab76f44a3c86
alliance <alliancekubwayo@gmail,com>
2026-08-26 09:48:24 +0200

    Ajout du formulaire connecté à Firebase

---
+++ script.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCj1mmOM2nV5HBHVk0-Iec4FKqqZfz0A9M",
+  authDomain: "st-project-91bd9.firebaseapp.com",
+  projectId: "st-project-91bd9",
+  storageBucket: "st-project-91bd9.firebasestorage.app",
+  messagingSenderId: "304249842658",
+  appId: "1:304249842658:web:5e7cee4dbd052544fc67bb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
